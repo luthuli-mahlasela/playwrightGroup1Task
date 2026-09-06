118931c490aa12441fed84558ef7e9a9aeb903f8
luthulim <luthuli_mahlasela@yahoo.com>
2026-09-06 21:22:44 +0200

    Added code to LoginPage

---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,21 @@
+import {Locator, expect} from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+
+    async openNdosiPage() {
+        await this.basePageGoToUrl('/');
+
+    }
+
+    async navigateToLoginPage() {
+        await this.basePageClickElement(this.page.getByRole('button', { name: 'Login' }));
+        await expect(this.page.locator('#login-email')).toBeVisible();
+    }
+
+       async userLogin(username: string, password: string) {
+        await this.basePageEnterText(this.page.locator('#login-email'), username);
+        await this.basePageEnterText(this.page.locator('#login-password'), password);
+        await this.basePageClickElement(this.page.locator('xpath=//button[contains(.,"Login")]'));
+    }
+}
